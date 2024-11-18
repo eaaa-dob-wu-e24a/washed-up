743cba7056a8cf9d27a0b160025988d7debaf146
frbarbre <fr.barbre@gmail.com>
2024-11-18 14:34:12 +0100

    added something to frontend

--- frontend-svelte/src/routes/+page.svelte
+++ frontend-svelte/src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Button from "@/components/ui/button/button.svelte";
+    let count =  $state(0);
 </script>
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<Button>
-    Press me baby!
+<a href="/about">Go to About</a>
+
+<Button onclick={() => count++}>
+    Press me baby! {count}
 </Button>
