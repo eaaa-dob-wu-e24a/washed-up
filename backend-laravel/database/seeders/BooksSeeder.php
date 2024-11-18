94112eba5ff5374b513754efe3948dd085eea2b6
Christoffer Larsen <itsgonnabealongnight@gmail.com>
2024-11-18 13:39:28 +0100

    Added example REST API

---
+++ backend-laravel/database/seeders/BooksSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Books;
+
+class BooksSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     */
+    public function run() {
+        $faker = \Faker\Factory::create();
+        for ($i = 0; $i < 10; $i++) {
+            Books::create([
+                'title' => $faker->sentence,
+                'author' => $faker->name,
+                'description' => $faker->paragraph
+            ]);
+        }
+    }
+}
