d60e9c97a7f696bb988e3220c1b5062ec7b97069
frbarbre <fr.barbre@gmail.com>
2024-11-19 13:40:20 +0100

    cleaned up

--- backend-laravel/bootstrap/app.php
+++ backend-laravel/bootstrap/app.php
@@ -6,13 +6,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-<<<<<<< Updated upstream
         api: __DIR__ . '/../routes/api.php',
-=======
-        web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
-        commands: __DIR__ . '/../routes/console.php',
->>>>>>> Stashed changes
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
