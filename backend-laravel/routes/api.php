68566aaedd0bd4e7030a7842a6977822879b3d48
Christoffer Larsen <itsgonnabealongnight@gmail.com>
2024-11-19 12:40:52 +0100

    Removed routes

--- backend-laravel/routes/api.php
+++ backend-laravel/routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+// use Illuminate\Http\Request;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::get('/books', [BookController::class, 'index']);
 Route::get('/books/{id}', [BookController::class, 'show']);
