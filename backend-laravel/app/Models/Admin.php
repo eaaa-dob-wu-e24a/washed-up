83281d745af34df9a663fd935114bf67187bc6b3
Christoffer Larsen <itsgonnabealongnight@gmail.com>
2024-11-21 10:01:16 +0100

    Adjusted admin

--- backend-laravel/app/Models/Admin.php
+++ backend-laravel/app/Models/Admin.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Admin extends Model {
     protected $fillable = [
         'name',
-        'role',
+        'location',
     ];
 
     public function reports() {
