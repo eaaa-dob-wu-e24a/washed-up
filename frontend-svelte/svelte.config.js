ecb8a96951ed52189866a3c919bd92244ca7fb17
frbarbre <fr.barbre@gmail.com>
2024-11-21 01:03:08 +0100

    switched to node adapter

--- frontend-svelte/svelte.config.js
+++ frontend-svelte/svelte.config.js
@@ -1,5 +1,5 @@
 // svelte.config.js
-import adapter from 'svelte-adapter-bun';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,9 +13,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
-		csrf: {
-			checkOrigin: false
-		},
+		// csrf: {
+		// 	checkOrigin: false
+		// },
 		alias: {
 			'@/*': './src/lib/*'
 		}
