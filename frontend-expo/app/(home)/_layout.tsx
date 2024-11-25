e1df452ada1d7232af9630ddcc898023ca465649
frbarbre <fr.barbre@gmail.com>
2024-11-25 13:57:58 +0100

    made clean up

--- frontend-expo/app/(home)/_layout.tsx
+++ frontend-expo/app/(home)/_layout.tsx
@@ -1,10 +1,7 @@
-import { useAuth, useUser } from "@clerk/clerk-expo";
+import { useUser } from "@clerk/clerk-expo";
 import { Redirect, Tabs } from "expo-router";
-import { Stack } from "expo-router/stack";
-import { Button } from "react-native";
 
 export default function Layout() {
-  const { signOut } = useAuth();
   const { user } = useUser();
 
   if (!user) {
@@ -15,7 +12,7 @@ export default function Layout() {
     <Tabs>
       <Tabs.Screen name="dashboard" />
       <Tabs.Screen name="my-page" />
-      <Tabs.Screen name="my-qr" />
+      <Tabs.Screen name="qr" />
     </Tabs>
   );
 }
--- frontend-expo/app/(home)/my-page.tsx
+++ frontend-expo/app/(home)/my-page.tsx
@@ -1,10 +1,23 @@
+import { useAuth, useUser } from "@clerk/clerk-expo";
+import { Redirect } from "expo-router";
 import { View } from "react-native";
+import { Button } from "~/components/ui/button";
 import { Text } from "~/components/ui/text";
 
 export default function MyPage() {
+  const { signOut } = useAuth();
+  const { user } = useUser();
+
+  if (!user) {
+    return <Redirect href={"/"} />;
+  }
+
   return (
     <View>
       <Text>My Page</Text>
+      <Button onPress={() => signOut()}>
+        <Text>Sign Out</Text>
+      </Button>
     </View>
   );
 }
