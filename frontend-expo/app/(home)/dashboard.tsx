894c11d31c979b64aea41b4584fa83e360de16a6
Christoffer Larsen <itsgonnabealongnight@gmail.com>
2024-11-28 17:20:03 +0100

    Added schedule styling

--- frontend-expo/app/(home)/dashboard.tsx
+++ frontend-expo/app/(home)/dashboard.tsx
@@ -2,13 +2,17 @@ import { Api } from "api";
 import { Machine } from "types";
 import { useUser } from "@clerk/clerk-expo";
 import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { Text } from "~/components/ui/text";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Heading from "~/components/heading";
+import ScheduleCard from "~/components/schedule-card";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function Dashboard() {
   const { user } = useUser();
   const [machines, setMachines] = useState<Machine[]>([]);
+  const [schedule, setSchedule] = useState(true);
 
   useEffect(() => {
     const token = user?.publicMetadata?.access_token;
@@ -25,12 +29,24 @@ export default function Dashboard() {
   return (
     <SafeAreaView className="p-6 h-screen justify-between">
       <View className="gap-4">
-        <Heading title="Hello!" />
-        <View>
+        <Heading title={`Hello, ${user?.emailAddresses}`} />
+        <Text className="text-2xl">Schedule</Text>
+        {schedule ? (
+          <ScrollView horizontal>
+            <View className="flex flex-row gap-4">
+              <ScheduleCard />
+              <ScheduleCard />
+            </View>
+          </ScrollView>
+        ) : (
+          <Text>You currently don't have any scheduled times!</Text>
+        )}
+        {/* <View>
           {machines?.map((machine) => (
             <Text key={machine.id}>{machine.model}</Text>
           ))}
-        </View>
+        </View> */}
+        <Text className="text-2xl">All machines</Text>
       </View>
     </SafeAreaView>
   );
---
+++ frontend-expo/components/schedule-card.tsx
@@ -0,0 +1,30 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Text } from "~/components/ui/text";
+
+export default function ScheduleCard() {
+  return (
+    <>
+      <Card className="w-72">
+        <CardHeader>
+          <CardTitle>Washer</CardTitle>
+          <CardDescription>Machine #13</CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col items-start">
+          <Text>Tuesday, 3 dec</Text>
+          <Text>12:00 - 15:00</Text>
+        </CardContent>
+        <CardFooter className="flex flex-col items-start">
+          <Text>Starts in: 12m 42s</Text>
+          <Text>Status in: Occupied</Text>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
