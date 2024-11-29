8b27c80858214e617cc5f30e578fbb2d31eee21c
Christoffer Larsen <itsgonnabealongnight@gmail.com>
2024-11-29 12:20:25 +0100

    Updated machine seeder

--- backend-laravel/database/seeders/MachineSeeder.php
+++ backend-laravel/database/seeders/MachineSeeder.php
@@ -19,9 +19,19 @@ class MachineSeeder extends Seeder {
             $locations = Location::factory()->count(10)->create();
         }
 
+        // Ensure each location has at least one machine
+        foreach ($locations as $location) {
+            Machine::create([
+                'type' => $faker->randomElement(['wash', 'dry']),
+                'location_id' => $location->id,
+                'status' => $faker->boolean,
+            ]);
+        }
+
+        // Create additional machines
         for ($i = 0; $i < 100; $i++) {
             Machine::create([
-                'model' => $faker->randomElement(['wash', 'dry']),
+                'type' => $faker->randomElement(['wash', 'dry']),
                 'location_id' => $locations->random()->id,
                 'status' => $faker->boolean,
             ]);
