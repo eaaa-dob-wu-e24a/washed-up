19f4e93fd9a24e992d0331c8e3fdd8efae13e345
Christoffer Larsen <itsgonnabealongnight@gmail.com>
2024-12-04 10:30:08 +0100

    Don't allow overlapping times

--- backend-laravel/app/Http/Controllers/ScheduleController.php
+++ backend-laravel/app/Http/Controllers/ScheduleController.php
@@ -17,11 +17,19 @@ class ScheduleController extends Controller {
     }
 
     public function store(Request $request) {
+        if ($this->hasOverlap($request->machine_id, $request->start_time, $request->end_time)) {
+            return response()->json(['error' => 'Schedule overlaps with an existing entry'], 409);
+        }
+
         $schedule = Schedule::create($request->all());
         return response()->json($schedule, 201);
     }
 
     public function update(Request $request, $id) {
+        if ($this->hasOverlap($request->machine_id, $request->start_time, $request->end_time, $id)) {
+            return response()->json(['error' => 'Schedule overlaps with an existing entry'], 409);
+        }
+
         $schedule = Schedule::findOrFail($id);
         $schedule->update($request->all());
         return response()->json($schedule, 200);
@@ -30,5 +38,23 @@ class ScheduleController extends Controller {
     public function destroy($id) {
         Schedule::destroy($id);
         return response()->json(null, 204);
+    }
+
+    private function hasOverlap($machine_id, $start_time, $end_time, $exclude_id = null) {
+        $query = Schedule::where('machine_id', $machine_id)
+            ->where(function ($query) use ($start_time, $end_time) {
+                $query->where('start_time', '<', $end_time)
+                    ->where('end_time', '>', $start_time)
+                    ->orWhere(function ($query) use ($start_time, $end_time) {
+                        $query->where('start_time', '<', $start_time)
+                            ->where('end_time', '>', $end_time);
+                    });
+            });
+
+        if ($exclude_id) {
+            $query->where('id', '!=', $exclude_id);
+        }
+
+        return $query->exists();
     }
 }
